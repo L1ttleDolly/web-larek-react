04e6d0ffb9159df5ecea1df02e6a00e7462382a0
Vitaliya <vitaliya_malyutina@mail.ru>
2026-01-16 16:39:08 +0300

    feature: верстка формы

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,43 +1,11 @@
 import styles from "./mainPage.module.scss";
-
 import { Gallery } from "../../widgets/Gallery/Gallery.tsx";
-import { Modal } from "../../widgets/Modal/Modal.tsx";
-import { useDispatch, useSelector } from "../../app/store/store.ts";
-import { CardModal } from "../../widgets/CardModal/CardModal.tsx";
-import { closeModal } from "../../entities/modal/model/modalSlice.ts";
-import { FormOrder } from "../../widgets/FormOrder/FormOrder.tsx";
-import { Basket } from "../../widgets/Basket/Basket.tsx";
+import { ModalRoot } from "../../widgets/ModalRoot/ModalRoot.tsx";
 
 export const MainPage = () => {
-  const selectedCard = useSelector((state) => state.modal.payload);
-
-  const open = useSelector((state) => state.modal.isOpen);
-  const type = useSelector((state) => state.modal.type);
-
-  console.log("open", open);
-  console.log("type:", type);
-
-  const product = useSelector((state) => state.products.products[selectedCard]);
-  const dispatch = useDispatch();
-
   return (
     <main className={styles.container}>
-      {open && (
-        <Modal isOpen={open} onClose={() => dispatch(closeModal())}>
-          {type === "basket" && <Basket />}
-          {type === "card" && (
-            <CardModal
-              title={product.title}
-              price={product.price}
-              category={product.category}
-              image={product.image}
-              id={product.id}
-              description={product.description}
-            />
-          )}
-          {type === "form" && <FormOrder />}
-        </Modal>
-      )}
+      <ModalRoot />
       <Gallery />
     </main>
   );
