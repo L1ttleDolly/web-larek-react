6e96b5a04e79e678f208f794e87d1846eb7310d9
Vitaliya <vitaliya_malyutina@mail.ru>
2026-01-16 12:50:58 +0300

    feature: удаление карточки из корзины через модалку, валидация кнопки, проверка на наличие карточки в корзине

--- src/entities/basket/model/basketSlice.ts
+++ src/entities/basket/model/basketSlice.ts
@@ -2,12 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 
 type TInitialState = {
   items: string[];
-  price: number;
 };
 
 const initialState: TInitialState = {
   items: [],
-  price: 0,
 };
 
 export const basketSlice = createSlice({
@@ -21,11 +19,7 @@ export const basketSlice = createSlice({
     deleteItem(state, action) {
       state.items = state.items.filter((item) => item !== action.payload);
     },
-
-    setPrice(state, action) {
-      state.price = state.price + action.payload;
-    },
   },
 });
 
-export const { addItem, deleteItem, setPrice } = basketSlice.actions;
+export const { addItem, deleteItem } = basketSlice.actions;
