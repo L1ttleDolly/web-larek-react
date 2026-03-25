228b33dd4c7c6c5f3dba2734f13e17b723c303ec
Vitaliya <vitaliya_malyutina@mail.ru>
2026-03-26 00:57:00 +0300

    fix

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import LogoIcon from "../../assets/images/icon/logo.svg";
 
 export const Logo = () => {
   return (
-    <a href="/public">
+    <a href="/">
       <img src={LogoIcon} alt="" className={styles.logo} />
     </a>
   );
